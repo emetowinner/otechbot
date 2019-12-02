2606319130d570ef534014832d32dc5d30ddff48
emetowinner <emetowinner@gmail.com>
2019-12-02 09:20:07 +0100

    update

--- bot/server.py
+++ bot/server.py
@@ -1,5 +1,5 @@
-from os import environ
-from flask import Flask
+# from os import environ
+# from flask import Flask
 
-app =Flask(__name__)
-app.run(host= '0.0.0.0',port=environ.get('PORT'))
+# app =Flask(__name__)
+# app.run(host= '0.0.0.0',port=environ.get('PORT'))
