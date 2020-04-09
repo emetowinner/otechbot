e136a4c5c2f6f9007fef9778b39797406bed6c7e
emetowinner <emetowinner@gmail.com>
2020-04-09 16:09:31 -0700

    Update the bot to respond to the COVID-19 outbreak

--- bot/bot.py
+++ bot/bot.py
@@ -6,6 +6,7 @@ import time
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
+
 class RetweetListener(tweepy.StreamListener):
     def __init__(self, api):
         self.api = api
@@ -14,52 +15,50 @@ class RetweetListener(tweepy.StreamListener):
     def on_status(self, tweet):
         logger.info(f"Processing tweet id {tweet.id}")
         if tweet.in_reply_to_status_id is not None or \
-            tweet.user.id == self.me.id:
-            # Ignore the tweet because is a reply or I'm its author 
+                tweet.user.id == self.me.id:
+            # Ignore the tweet because is a reply or I'm its author
             return
         if not tweet.favorited:
             # Mark it as Liked
             try:
                 tweet.favorite()
             except Exception as e:
-                logger.error("Error occured while liking the tweet", exc_info=True)
+                logger.error(
+                    "Error occured while liking the tweet", exc_info=True)
                 print(e)
         if not tweet.retweeted:
             # Retweet, if it haven't retweeted yet
             try:
                 tweet.retweet()
                 logger.info(f"Repling tweet id {tweet.id}")
-                if 'COVID-19' in tweet_post.text:
-                    self.api.update_status(f'@{tweet.user.screen_name} STAY SAFE! STAY AT HOME!! @mtechdevimo @WPowerri @owerriTechHub @OluakaInstitute @LaravelOwerri @WomenProTech1 @pyladiesimo @dscimsu @Genesysclubfuto @BleauTechOrg ', in_reply_to_status_id=tweet.id)
-                    self.api.update_status(f'@{tweet.user.screen_name} @oscaimo @ingressiveIMSU @dsc_futo @dotnetseNG @mtcowerri @gdgowerri @thaimpactcircle @losintech', in_reply_to_status_id=tweet.id)
-                    tweet_post = self.api.get_status(id=tweet.id)
-                else:                    
-                    self.api.update_status(f'@{tweet.user.screen_name} @mtechdevimo @WPowerri @owerriTechHub @OluakaInstitute @LaravelOwerri @WomenProTech1 @pyladiesimo @dscimsu @Genesysclubfuto @BleauTechOrg ', in_reply_to_status_id=tweet.id)
-                    self.api.update_status(f'@{tweet.user.screen_name} @oscaimo @ingressiveIMSU @dsc_futo @dotnetseNG @mtcowerri @gdgowerri @thaimpactcircle ', in_reply_to_status_id=tweet.id)
-                    tweet_post = self.api.get_status(id=tweet.id)
+                self.api.update_status(
+                    f'@{tweet.user.screen_name} [[STAY SAFE! STAY AT HOME!! Visit (http://covid19.ncdc.gov.ng) for updates]] @mtechdevimo @WPowerri @owerriTechHub @OluakaInstitute @LaravelOwerri @WomenProTech1 @pyladiesimo @dscimsu @Genesysclubfuto @BleauTechOrg ', in_reply_to_status_id=tweet.id)
+                self.api.update_status(
+                    f'@{tweet.user.screen_name} @oscaimo @ingressiveIMSU @dsc_futo @dotnetseNG @mtcowerri @gdgowerri @thaimpactcircle @losintech', in_reply_to_status_id=tweet.id)
 
             except Exception:
                 logger.error("Error on fav and retweet", exc_info=True)
-                
-        try:                    
+
+        try:
             logger.info("Retrieving and following followers")
             for follower in tweepy.Cursor(self.api.followers).items():
                 if not follower.following:
                     logger.info(f"Following {follower.name}")
-                    follower.follow()     
-        except Exception:       
-            logger.error("Error occured trying to follow", exc_info=True) 
-            pass       
+                    follower.follow()
+        except Exception:
+            logger.error("Error occured trying to follow", exc_info=True)
+            pass
 
     def on_error(self, status):
         logger.error(status)
 
-    
+
 def main(keywords):
     api = create_api()
     tweets_listener = RetweetListener(api)
     stream = tweepy.Stream(api.auth, tweets_listener)
     stream.filter(track=keywords, languages=["en"])
 
+
 if __name__ == "__main__":
-    main(["OwerriTech","owerritech","Owerritech","owerriTech"])
+    main(["OwerriTech", "owerritech", "Owerritech", "owerriTech","imo covid19","imo covid-19","IMO COVID19","IMO COVID-19"])
